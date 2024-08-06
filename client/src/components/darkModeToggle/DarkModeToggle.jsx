682502c8e494ab4a5228f543bf9600e2699cce38
YasinzHyper <zulfiyasser@gmail.com>
2024-08-06 21:11:26 +0530

    Implement dark theme and responsiveness

--- client/src/components/darkModeToggle/DarkModeToggle.jsx
+++ client/src/components/darkModeToggle/DarkModeToggle.jsx
@@ -1,40 +1,56 @@
 import React, { useState, useEffect } from "react";
 import Toggle from "react-toggle";
-// import "react-toggle/style.css"; // import the toggle style
 import "./DarkModeToggle.scss";
+import { useMediaQuery } from "react-responsive";
 
 const DarkModeToggle = () => {
+  const prefersDarkMode = useMediaQuery({
+    query: "(prefers-color-scheme: dark)",
+  });
+
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    // Check if the user has a preferred theme
     const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark") {
-      document.body.classList.add("dark");
-      setIsDarkMode(true);
-    }
-  }, []);
 
-  const handleToggleChange = () => {
-    setIsDarkMode(!isDarkMode);
-    if (!isDarkMode) {
+    if (savedTheme) {
+      setIsDarkMode(savedTheme === "dark");
+      setPreferredColorScheme(savedTheme);
+    } else if (prefersDarkMode) {
+      setIsDarkMode(true);
+      setPreferredColorScheme("dark");
+    }
+  }, [prefersDarkMode]);
+
+  const setPreferredColorScheme = (theme) => {
+    if (theme === "dark") {
       document.body.classList.add("dark");
       localStorage.setItem("theme", "dark");
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches === false) {
+        document.body.style.setProperty("color-scheme", "dark");
+      }
     } else {
       document.body.classList.remove("dark");
       localStorage.setItem("theme", "light");
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches === true) {
+        document.body.style.setProperty("color-scheme", "light");
+      }
     }
   };
 
+  const handleToggleChange = () => {
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode);
+    setPreferredColorScheme(newMode ? "dark" : "light");
+  };
+
   return (
-    <div>
-      <Toggle
-        defaultChecked={isDarkMode}
-        onChange={handleToggleChange}
-        icons={{ checked: "🌜", unchecked: "🌞" }}
-        aria-label="Dark mode toggle"
-      />
-    </div>
+    <Toggle
+      defaultChecked={isDarkMode}
+      onChange={handleToggleChange}
+      icons={{ checked: "🌖", unchecked: "🔆" }}
+      aria-label="Dark mode toggle"
+    />
   );
 };
 
