d17a28490feaf0d3514063e0070589eeddb87a35
YasinzHyper <zulfiyasser@gmail.com>
2024-08-07 21:42:45 +0530

    deployment

--- api/app.js
+++ api/app.js
@@ -1,26 +1,35 @@
-import express from 'express';
-import postRouter from './routes/post_route.js';
-import testRouter from './routes/test_route.js';
-import authRouter from './routes/auth_route.js';
-import userRouter from './routes/user_route.js';
-import chatRouter from './routes/chat_route.js';
-import messageRouter from './routes/message_route.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import postRouter from "./routes/post_route.js";
+import testRouter from "./routes/test_route.js";
+import authRouter from "./routes/auth_route.js";
+import userRouter from "./routes/user_route.js";
+import chatRouter from "./routes/chat_route.js";
+import messageRouter from "./routes/message_route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
-app.use(cors({origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/posts', postRouter);
-app.use('/api/users', userRouter);
-app.use('/api/test', testRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/chats', chatRouter);
-app.use('/api/messages', messageRouter);
+app.get("/", (req, res) => {
+  res.json("Hello World");
+});
+
+app.use("/api/posts", postRouter);
+app.use("/api/users", userRouter);
+app.use("/api/test", testRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/chats", chatRouter);
+app.use("/api/messages", messageRouter);
 
 app.listen(8800, () => {
-    console.log('Server is running on port 8800');
-})
+  console.log("Server is running on port 8800");
+});
