f5c7ab13c8053e2285f438a6795556c0a5d2413b
YasinzHyper <zulfiyasser@gmail.com>
2024-08-07 21:41:01 +0530

    cors

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ import cors from 'cors';
 
 const app = express();
 
-// app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
+app.use(cors({origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
