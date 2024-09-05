161f9ef95e418d04bbe809c4cd47c3f4f10557d7
YasinzHyper <zulfiyasser@gmail.com>
2024-09-05 19:50:22 +0530

    modify prisma

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
+} else {
+    prisma = new PrismaClient();
+}
 
 export default prisma;
