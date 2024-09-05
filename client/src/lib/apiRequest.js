e5d424de556b927a4a05e5b6a047d412d191670d
YasinzHyper <zulfiyasser@gmail.com>
2024-09-05 19:00:45 +0530

    api url

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8800/api", // process.env.REACT_APP_API_URL ||
   withCredentials: true,
 });
 
